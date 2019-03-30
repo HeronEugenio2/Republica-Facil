76b2592313e3d5342cf882dfa815ad09f09055ee
Heron <hrs.eugenio@gmail.com>
2019-03-30 12:34:55 -0300

    seeder aletracao migrate

---
+++ app/Http/Controllers/UserUserInformationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UserUserInformation;
+use Illuminate\Http\Request;
+
+class UserUserInformationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UserUserInformation  $userUserInformation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserUserInformation $userUserInformation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UserUserInformation  $userUserInformation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserUserInformation $userUserInformation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UserUserInformation  $userUserInformation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserUserInformation $userUserInformation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UserUserInformation  $userUserInformation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserUserInformation $userUserInformation)
+    {
+        //
+    }
+}
---
+++ database/seeds/DataTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Type;
+use Illuminate\Database\Seeder;
+
+/**
+ * Class DataTableSeeder
+ */
+class DataTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Type::create([
+                         'title'      => 'Masculina',
+                     ]);
+        Type::create([
+                         'title'      => 'Feminina',
+                     ]);
+        Type::create([
+                         'title'      => 'Mista',
+                     ]);
+    }
+}

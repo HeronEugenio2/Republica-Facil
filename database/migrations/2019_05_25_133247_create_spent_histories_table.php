aa659578ef7950329d593318941a2de1ce3b05d7
Heron <hrs.eugenio@gmail.com>
2019-05-25 11:37:32 -0300

    migration

---
+++ database/migrations/2019_05_25_133247_create_spent_histories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpentHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spent_histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('month');
+            $table->string('value');
+            $table->unsignedInteger('republic_id')->nullable();
+            $table->unsignedInteger('user_id')->nullable();
+            $table->boolean('buy')->default(0);
+            $table->timestamps();
+        });
+        Schema::table('spent_histories', function(Blueprint $table) {
+            $table->foreign('republic_id')->references('id')->on('republics');
+        });
+        Schema::table('spent_histories', function(Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spent_histories');
+    }
+}

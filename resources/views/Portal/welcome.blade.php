dc91e26cb0e9c62f56613457859e2a31ecb3a4bf
Heron <hrs.eugenio@gmail.com>
2019-06-02 03:32:34 -0300

    portal

--- resources/views/Portal/welcome.blade.php
+++ resources/views/Portal/welcome.blade.php
@@ -50,7 +50,7 @@
                         <div class="col-md-4">
                             <i class="text-danger fas fa-search-location fa-3x mb-2"></i>
                             <h5 class='text-danger'>Encontre lugares para morar</h5>
-                            <p class='text-muted' >
+                            <p class='text-muted'>
                                 Use nossa busca para encontrar excelentes apartamentos para dividir aluguel e quartos para alugar em todas as regiões do Brasil
                             </p>
                         </div>
@@ -59,9 +59,9 @@
                             <h5 class='text-danger'>
                                 Negocie diretamente com o anunciante
                             </h5>
-                                <p class='text-muted'>
-                                    Utilize nossa plataforma para trocar mensagens e conhecer melhor a pessoa que irá dividir moradia com você.
-                                </p>
+                            <p class='text-muted'>
+                                Utilize nossa plataforma para trocar mensagens e conhecer melhor a pessoa que irá dividir moradia com você.
+                            </p>
                         </div>
                         <div class="col-md-4">
                             <i class="text-danger fas fa-key fa-3x mb-2"></i>
@@ -77,33 +77,35 @@
             </div>
         </div>
     </div>
-    <div class="album py-5 bg-light">
-        <div class="container">
-            <h1 class='text-center mb-2'>Anúncios em Destaque</h1>
-            <div class="row">
-                @foreach($republics as $republic)
-                    <div class="col-md-4">
-                        <div class="card mb-4 box-shadow">
-                            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Miniatura [100% x225]" style="height: 225px; width: 100%; display: block;" src="{{$republic->image}}" data-holder-rendered="true">
-                            <div class="card-body">
-                                <p class="card-text">
-                                <h4>{!! \Illuminate\Support\Str::limit($republic->name, 20) !!}</h4>
-                                <p>{!! \Illuminate\Support\Str::limit($republic->description, 80) !!}</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">
-                                            <font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Visão</font></font>
-                                        </button>
-                                        <button type="button" class="btn btn-sm btn-outline-success">Contato</button>
+    <div class='jumbotron jumbotron-fluid mb-0'>
+        <div class="album">
+            <div class="container">
+                <h1 class='text-center mb-2'>Anúncios em Destaque</h1>
+                <div class="row">
+                    @foreach($republics as $republic)
+                        <div class="col-md-4">
+                            <div class="card mb-4 box-shadow shadow">
+                                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Miniatura [100% x225]" style="height: 225px; width: 100%; display: block;" src="{{$republic->image}}" data-holder-rendered="true">
+                                <div class="card-body">
+                                    <p class="card-text">
+                                    <h4>{!! \Illuminate\Support\Str::limit($republic->name, 20) !!}</h4>
+                                    <p>{!! \Illuminate\Support\Str::limit($republic->description, 80) !!}</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                            <button type="button" class="btn btn-sm btn-outline-secondary">
+                                                <font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Visão</font></font>
+                                            </button>
+                                            <button type="button" class="btn btn-sm btn-outline-success">Contato</button>
+                                        </div>
+                                        <small class="text-muted">
+                                            <font style="vertical-align: inherit;"><font style="vertical-align: inherit;">9 min</font></font>
+                                        </small>
                                     </div>
-                                    <small class="text-muted">
-                                        <font style="vertical-align: inherit;"><font style="vertical-align: inherit;">9 min</font></font>
-                                    </small>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
